9ddaeb47b0287f81a81d7e601401fea16aac85a6
Malte Jansen <m.jansen@madsack-mediastore.de>
2016-02-18 01:07:46 +0100

    [add] theme svlaatzen

---
+++ addons_custom/theme_svlaatzen/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Theme for SV Laatzen",
+    'version': "0.1",
+    'author': "Malte Jansen",
+    'license': 'AGPL-3',
+    'category': "Theme",
+    'summary': "",
+    "description": """
+
+    """,
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/themes.xml',
+    ],
+    'installable': True,
+}
